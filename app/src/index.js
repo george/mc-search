230cf6d6e0ee1ffaeac2502903cda02ed58c5bf0
George <sam.polshek@gmail.com>
2022-10-19 16:25:47 -0500

    Added search and loading pages

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, ipcMain, BrowserWindow } = require('electron')
+const axios = require('axios')
 const isDev = require('electron-is-dev');
 
 let mainWindow;
@@ -7,6 +8,11 @@ const createMainWindow = () => {
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 720,
+
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     })
 
     //Disable menu bar
@@ -15,14 +21,20 @@ const createMainWindow = () => {
     //Load HTML file into the window
     mainWindow.loadFile('./resources/views/index.html')
 
-    //Set the icon for development build
-    mainWindow.setIcon('./resources/icon/logo.png')
-
     //When the main window is closed, quit the application
     mainWindow.on('closed', () => {
         app.quit()
     })
 }
+
+ipcMain.on('search', (event, query) => {
+    mainWindow.loadFile('./resources/views/loading.html')
+
+    axios.get((isDev ? 'http://127.0.0.1:3000' : 'https://api.hostile.org') + '/data/' + query)
+        .then(response => {
+            console.log(response)
+        })
+})
 
 app.whenReady().then(() => {
     createMainWindow()
