2afa6d5e0aa94a1c4c2b76f3cf1eae807534c079
George <sam.polshek@gmail.com>
2022-10-18 21:51:20 -0500

    Added base for Flask project

--- server/app.py
+++ server/app.py
@@ -1,2 +1,20 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
 from flask import Flask
 
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route('/data/<username>')
+def handle_main_route(username):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('HOST'), port=os.getenv('PORT'))
